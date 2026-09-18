eef67ab6d422d1d052730ed06ae6676cf8edec5f
Rohan Bendapudi <datanteater@gmail.com>
2026-09-18 14:53:16 -0400

    fixed error --> properly initailized state as a ROS message, and created a separate function to subscribe to state

--- ros_behaviors_fsm/ros_behaviors_fsm/drive_arch.py
+++ ros_behaviors_fsm/ros_behaviors_fsm/drive_arch.py
@@ -9,16 +9,22 @@ from math import pi
 class DriveArchNode(Node):
     def __init__(self):
         super().__init__("drive_arch_node")
+        self.state = Int8()
         self.time_per_turn = 1
         self.timer = self.create_timer(self.time_per_turn, self.run_loop)
         self.linear_distance = 0.1
         self.angular = pi / 30
         self.vel_pub = self.create_publisher(Twist, "/cmd_vel", 10)
-        self.state_sub = self.create_subscription(Int8, "/fsm_state", self.run_loop, 10)
+        self.state_sub = self.create_subscription(
+            Int8, "/fsm_state", self.state_tracker, 10
+        )
 
-    def run_loop(self, state: Int8):
+    def state_tracker(self, state: Int8):
+        self.state = state
 
-        if state == 1:
+    def run_loop(self):
+
+        if self.state.data == 1:
             msg = Twist()
             msg.linear.x = self.linear_distance / self.time_per_turn
             msg.angular.z = self.angular / self.time_per_turn
