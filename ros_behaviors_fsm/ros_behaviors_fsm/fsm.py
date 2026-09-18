708577dad2f86e27ce626791e65c2d220672df5e
Rohan Bendapudi <datanteater@gmail.com>
2026-09-18 12:42:52 -0400

    created a FSM to manage between states, with each node acting as its own state

---
+++ ros_behaviors_fsm/ros_behaviors_fsm/fsm.py
@@ -0,0 +1,50 @@
+import numpy as np
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Int8, Bool
+from math import pi
+from enum import Enum
+
+
+class STATE(Enum):
+    DRIVE_ARCH = 1
+    BUMP_DETECTED = 2
+    FOLLOW_PERSON = 3
+
+
+class FSMNode(Node):
+    def __init__(self):
+        super.__init__("fsm")
+        self.state = STATE.DRIVE_ARCH
+        self.time_per_turn = 0.1
+        self.found_following_state = False
+        self.bumped_reversing_state = False
+        self.create_timer(
+            self.time_per_turn,
+        )
+        self.fsm_state_pub = self.create_publisher(Int8, "/fsm_state", 10)
+        self.create_subscription(Bool, "/found_following_state", self.follower_sub, 10)
+        self.create_subscription(Bool, "/bumped_reversing", self.bumped_sub, 10)
+
+    # subscription created for follower
+    def follower_sub(self, msg: Bool):
+        self.found_following_state = msg
+
+    # subscription created for bumped msg
+    def bumped_sub(self, msg: Bool):
+        self.bumped_reversing_state = msg
+
+    def run_loop(self):
+
+        # state selector logic
+        if self.bumped_reversing_state & self.found_following_state:
+            self.state = STATE.BUMP_DETECTED
+        elif self.found_following_state:
+            self.state = STATE.FOLLOW_PERSON
+        elif self.bumped_reversing_state:
+            self.state = STATE.BUMP_DETECTED
+        else:
+            self.state = STATE.DRIVE_ARCH
+
+        self.fsm_state_pub.publish(self.state)
