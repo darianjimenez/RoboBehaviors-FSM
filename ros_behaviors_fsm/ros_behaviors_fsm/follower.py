f6ec60333c15b25c02f635f459bd032a92f2632b
Rohan Bendapudi <datanteater@gmail.com>
2026-09-18 12:51:56 -0400

    integrated FSm with follower.py, will only move in state 3, and will publish info to fsm if a target is found within LIDAR --> will then inform fsm, which will start people follower node

--- ros_behaviors_fsm/ros_behaviors_fsm/follower.py
+++ ros_behaviors_fsm/ros_behaviors_fsm/follower.py
@@ -1,14 +1,28 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Int8, Bool
 from geometry_msgs.msg import Twist
+
 
 class PeopleFollowNode(Node):
     def __init__(self):
         super().__init__("people_follow_node")
-        self.scan_sub = self.create_subscription(LaserScan, "/scan", self.scan_callback, 10)
-        self.vel_pub = self.create_publisher(Twist, "/cmd_vel", 10)
 
+        self.state = 0
+        self.scan_sub = self.create_subscription(
+            LaserScan, "/scan", self.scan_callback, 10
+        )
+        self.state_sub = self.create_subscription(
+            Int8, "/fsm_state", self.state_tracker, 10
+        )
+        self.vel_pub = self.create_publisher(Twist, "/cmd_vel", 10)
+        self.follower = self.create_publisher(Bool, "/found_following_target_", 10)
+
+    def state_tracker(self, state):
+        self.state = state
+
+    # current issue --> need to subscribe to multiple states in one node
     def scan_callback(self, scan):
         msg = Twist()
 
@@ -19,20 +33,27 @@ class PeopleFollowNode(Node):
         left = ranges[30]
         right = ranges[-30]
 
-        # Turn toward the closest side
-        if left < front and left < right:
-            msg.angular.z = 0.3
-        elif right < front and right < left:
-            msg.angular.z = -0.3
-        else:
-            msg.angular.z = 0.0
+        # checks if
+        if any(front, left, right != 0):
+            found_following_target = True
+            self.follower.publish(found_following_target)
 
-        if front > 0.8:
-            msg.linear.x = 0.1
-        else:
-            msg.linear.x = 0.0
+        if self.state == 3:
+            # Turn toward the closest side
+            if left < front and left < right:
+                msg.angular.z = 0.3
+            elif right < front and right < left:
+                msg.angular.z = -0.3
+            else:
+                msg.angular.z = 0.0
+
+            if front > 0.8:
+                msg.linear.x = 0.1
+            else:
+                msg.linear.x = 0.0
 
         self.vel_pub.publish(msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
