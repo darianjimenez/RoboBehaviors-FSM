1888e83c59e77191c6fe48ea60b9daa5c0eaef53
Rohan Bendapudi <datanteater@gmail.com>
2026-09-18 12:50:11 -0400

    integrated fsm into drive arch to only run when FSM state is set to 1

--- ros_behaviors_fsm/ros_behaviors_fsm/drive_arch.py
+++ ros_behaviors_fsm/ros_behaviors_fsm/drive_arch.py
@@ -2,7 +2,7 @@ import numpy as np
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Header
+from std_msgs.msg import Int8
 from math import pi
 
 
@@ -13,13 +13,16 @@ class DriveArchNode(Node):
         self.timer = self.create_timer(self.time_per_turn, self.run_loop)
         self.linear_distance = 0.1
         self.angular = pi / 30
-        self.vel_pub = self.create_publisher(Twist, "cmd_vel", 10)
+        self.vel_pub = self.create_publisher(Twist, "/cmd_vel", 10)
+        self.state_sub = self.create_subscription(Int8, "/fsm_state", self.run_loop, 10)
 
-    def run_loop(self):
-        msg = Twist()
-        msg.linear.x = self.linear_distance / self.time_per_turn
-        msg.angular.z = self.angular / self.time_per_turn
-        self.vel_pub.publish(msg)
+    def run_loop(self, state: Int8):
+
+        if state == 1:
+            msg = Twist()
+            msg.linear.x = self.linear_distance / self.time_per_turn
+            msg.angular.z = self.angular / self.time_per_turn
+            self.vel_pub.publish(msg)
 
 
 def main(args=None):
